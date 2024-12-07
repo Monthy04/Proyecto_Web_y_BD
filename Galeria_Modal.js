26e0f26ef6adf0f36ac12f0d5a1340ac11c68dd0
Christian Dael Silis Silvestre <spartan-0404@outlook.com>
2024-12-06 23:18:13 -0600

    commit

---
+++ Galeria_Modal.js
@@ -0,0 +1,44 @@
+﻿// Abrir Modal
+function Abrir_Modal() {
+  document.getElementById("Galeria_Modal").style.display = "block";
+}
+
+// Cerrar Modal
+function Cerrar_Modal() {
+  document.getElementById("Galeria_Modal").style.display = "none";
+}
+
+var Indice_Diapositiva = 1;
+Mostrar_Diapositivas(Indice_Diapositiva);
+
+// Next/previous controls
+function Cambiar_Diapositiva(n) {
+  Mostrar_Diapositivas(Indice_Diapositiva+= n);
+}
+
+// Thumbnail image controls
+function Diapositva_Actual(n) {
+  Mostrar_Diapositivas(Indice_Diapositiva= n);
+}
+
+function Mostrar_Diapositivas(n) {
+  var i;
+  var Diapositivas = document.getElementsByClassName("Diapositivas");
+  var NumeroDiapositiva = document.getElementById("DiapositivaNum");
+  if (n > Diapositivas .length) 
+  {
+  	Indice_Diapositiva= 1
+  }
+  if (n < 1) 
+  {
+  	Indice_Diapositiva= Diapositivas .length
+  }
+  
+  for (i = 0; i < Diapositivas .length; i++) 
+  {
+   	Diapositivas [i].style.display = "none";
+  }
+  Diapositivas [Indice_Diapositiva-1].style.display = "block";
+  NumeroDiapositiva .innerHTML =  Indice_Diapositiva + "/" + Diapositivas.length;
+ }
+ 
---
+++ gameplay_info.js
@@ -0,0 +1,12 @@
+﻿function Mostrar_Seccion(n) 
+{
+  var i;
+  var Secciones = document.getElementsByClassName("Contenedor_Informacion");
+  
+  for (i = 0; i < Secciones .length; i++) 
+  {
+   	Secciones [i].style.display = "none";
+  }
+  Secciones [n].style.display = "block";
+ }
+
